4a121788fc2c26a1869cdd3b6fb739eaaf3feb9d
User <User@DESKTOP-9SNQ1MG>
2021-08-11 21:15:46 -0500

    Esta es la primera version de mi proyecto para progarmacion orientada a objetos

--- src/ec/edu/intsuperior/vista/Aplicacion.java
+++ src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -2,14 +2,25 @@
 package ec.edu.intsuperior.vista;
 
 import ec.edu.intsuperior.modelo.Cliente;
+import ec.edu.intsuperior.modelo.Persona;
 
 
 
 public class Aplicacion {
     public static void main(String[] args) {
-    Cliente cliente1=new Cliente();
-    
-    cliente1.setTlf("09876543321");
+  Persona p1 = new Persona();
+  Cliente c1= new Cliente();
+        
+       
+        
+        p1.setNombre("Andres");
+        p1.setEdad(25);
+        c1.setTlf("0987654321");
+ 
+       System.out.println(p1.toString()+"\n"+
+               "Telefono:"+c1.getTlf() );
+
+        
     
     }
     
